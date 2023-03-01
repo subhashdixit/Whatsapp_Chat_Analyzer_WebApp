f45b10862a189256aece5e96cb8f0a6bb55f49f9
Subhash Dixit <subhashdixit17@gmail.com>
2023-03-01 19:19:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import nltk
 # nltk.download('all')
 # nltk.download('punkt')
 # nltk.download('stopwords')
-# nltk.download('vader_lexicon')
+nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
